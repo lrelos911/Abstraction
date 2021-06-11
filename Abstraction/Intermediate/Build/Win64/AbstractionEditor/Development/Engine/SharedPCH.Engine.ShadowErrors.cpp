841eef6a566d3d487e4620a86cd0b33ee5d38c19
Lorenzo <lorenzo.relos@live.com>
2021-06-11 01:04:34 -0700

    Abstraction uproject
    Initial project shell commit

---
+++ Abstraction/Intermediate/Build/Win64/AbstractionEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30037
+#include "C:/Users/Lorenzo.000/Documents/Unreal Projects/Abstraction/Abstraction/Intermediate/Build/Win64/AbstractionEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
